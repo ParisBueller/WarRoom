37b6f0684305cf1617c4eaef0ff5fad8034c77f9
Paris Ward <wardparis.pw@gmail.com>
2020-03-31 16:49:10 -0700

    Navbar, Dashboard, concurrent servers and proxy, auth action/reducer

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
+import * as actions from '../actions';
+import Navbar from './Navbar';
+import Dashboard from './Dashboard';
 
 class App extends React.Component {
+    componentDidMount() {
+      this.props.fetchUser();
+    }
+
   render() {
     return (
       <div className="container">
         <BrowserRouter>
           <div className="container">
-
+          <Navbar />
+          <Route exact path="/dashboard" component={Dashboard}></Route>
           </div>
         </BrowserRouter>
       </div>
@@ -16,7 +25,7 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default connect(null, actions)(App);
 
 
 
---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import { fetchUser } from '../actions/index';
+
+class Dashboard extends React.Component {
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+    renderDashboard() {
+        if (this.props.auth === null || false) {
+            return (
+                <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+            );
+        }
+        return (
+            <div>This is the dashboard</div>
+        )
+    }
+
+    render() {
+        return(
+            <div>
+                {this.renderDashboard()}
+            </div>
+        )
+    }
+}
+
+function mapStateToProps({ auth }) {
+    return { auth };
+}
+
+export default connect(mapStateToProps, { fetchUser})(Dashboard);
